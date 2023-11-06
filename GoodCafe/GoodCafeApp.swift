bc305b0f5292fc074694723d74d966bf94acb46c
klimentiuss <klimenko.apple01@icloud.com>
2023-11-06 19:35:12 +0200

    Initial Commit

---
+++ GoodCafe/GoodCafeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodCafeApp.swift
+//  GoodCafe
+//
+//  Created by Daniil Klimenko on 06.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GoodCafeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoodCafeUITests/GoodCafeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GoodCafeUITestsLaunchTests.swift
+//  GoodCafeUITests
+//
+//  Created by Daniil Klimenko on 06.11.2023.
+//
+
+import XCTest
+
+final class GoodCafeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
